1ebedb1d8df330434aaf93f2f46d1b978f017330
Andrei Dolmatov <mr.sociofob@gmail.com>
2023-12-07 14:02:47 +0100

    implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchString: string;
 }
 
 function App() {
@@ -29,7 +30,11 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchString) =>
+            setGameQuery({ ...gameQuery, searchString })
+          }
+        />
       </GridItem>
       <Show above="md">
         <GridItem area="aside" paddingX={5}>
